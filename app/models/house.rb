6287e145ec5d42999ae956e74f482f531295a2ff
Michael Pope <michael@dtcorp.com.au>
2013-01-17 11:55:51 +1100

    Allow deleting of houses with the _destroy text field

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,5 @@
 class House < ActiveRecord::Base
   attr_accessible :address, :cost, :postcode, :suburb, :value, :investor_id
-
+  
   belongs_to :investor
 end
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -2,7 +2,7 @@ class Investor < ActiveRecord::Base
   attr_accessible :name, :total_cost, :total_value, :houses_attributes
 
   has_many :houses
-  accepts_nested_attributes_for :houses
+  accepts_nested_attributes_for :houses, allow_destroy: true
 
   #
   # Override the as_json to allow updating of nested house attributes.
