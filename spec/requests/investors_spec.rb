a14fb52536186725fcd06fd10207a3782a915be5
Michael Pope <michael@dtcorp.com.au>
2013-01-21 14:13:15 +1100

    Test the javascript through phantomJS

--- spec/requests/investors_spec.rb
+++ spec/requests/investors_spec.rb
@@ -8,4 +8,13 @@ describe "Investors" do
       page.should have_content("Joseph Tramontana")
     end
   end
+  
+  describe "POST /investors", js: true do
+    it "creates an investor" do
+      visit angular_investors_path + "#" + new_investor_path
+      fill_in "Name", :with => "Amanda Powell"
+      click_button "Create Investor"
+      page.should have_content("Amanda Powell")
+    end
+  end
 end
