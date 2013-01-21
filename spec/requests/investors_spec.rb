3461fc27df9c890def6b4cff1329745ea5c6eb1a
Michael Pope <michael@dtcorp.com.au>
2013-01-21 14:58:40 +1100

    Add full javascript testing using headless phantomJS for creating a new investor

--- spec/requests/investors_spec.rb
+++ spec/requests/investors_spec.rb
@@ -12,7 +12,11 @@ describe "Investors" do
   describe "POST /investors", js: true do
     it "creates an investor" do
       visit angular_investors_path + "#" + new_investor_path
-      fill_in "Name", :with => "Amanda Powell"
+      fill_in "Name", with: "Amanda Powell"
+      fill_in "house_1_cost", with: "200000"
+      fill_in "house_1_value", with: "240000"
+      fill_in "house_2_cost", with: "700000"
+      fill_in "house_2_value", with: "680000"
       click_button "Create Investor"
       page.should have_content("Amanda Powell")
     end
